ae6f74aaa5847cfaf83eae3d676a6f5f2662b438
Abdul5412 <raoabdulrehman5412@gmail.com>
2025-07-13 03:52:33 -0700

    Added new file

---
+++ views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+
+def aboutUs(request):
+    return HttpResponse("Welcome to Wscubetech")
+def course(request):
+    return HttpResponse("Welcome to Wscubetech1")
